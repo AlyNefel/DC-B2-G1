3f1705c741ff1ef3bc523611ae422505a426023f
alynefel <ali.nefel2020@gmail.com>
2025-11-11 14:06:32 +0000

    javascript

---
+++ javascript/variables&DataTypes/variables.js
@@ -0,0 +1,22 @@
+// how to declare a variable
+var name ="ALY"
+console.log(name)
+// number datatype
+var number = 21543
+
+// boolean datatype 
+var isActive = true 
+
+//math operator
+console.log(6%2)
+// if condition
+var age =2
+
+if(age >18){
+    console.log("You can access the party !!")
+}else {
+    console.log("Go drink some milk and sleep !!")
+}
+
+//for loop  iterations
+
